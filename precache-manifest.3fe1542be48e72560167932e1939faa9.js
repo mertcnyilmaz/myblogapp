3806de23b7b867274a4b1ae0b7b31d7673673378
mertcnyilmaz <yilmazmertcannn@gmail.com>
2020-07-19 14:54:42 +0300

    Updates

---
+++ precache-manifest.3fe1542be48e72560167932e1939faa9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "870d8623eca20d6ddea26d20f1db208c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "65afb75dc1dd6b38469d",
+    "url": "./static/css/2.b6a8c48d.chunk.css"
+  },
+  {
+    "revision": "32953e2ec567f62b3cf3",
+    "url": "./static/css/main.166d5251.chunk.css"
+  },
+  {
+    "revision": "65afb75dc1dd6b38469d",
+    "url": "./static/js/2.002e9436.chunk.js"
+  },
+  {
+    "revision": "effe4e92e8658ad9dfbf53e804a021fb",
+    "url": "./static/js/2.002e9436.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32953e2ec567f62b3cf3",
+    "url": "./static/js/main.5308aa2c.chunk.js"
+  },
+  {
+    "revision": "5131e1ffed343f59682c",
+    "url": "./static/js/runtime-main.d364e9b8.js"
+  }
+]);
